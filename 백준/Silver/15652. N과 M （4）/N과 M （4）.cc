814ec9673ae7afb5e6fc294557a1736478e33fd4
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-05-16 11:13:39 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <stdlib.h>
+#include <algorithm>
+
+int n, m;
+int arr[8] = { 0, };
+int flag[9] = { 0, };
+void recursive(int level)
+{
+	int i;
+	if (level == m)
+	{
+		for (i = 0; i < m; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (level != 0)
+		{
+			if (flag[i + 1] == 0 && arr[level - 1] <= i+1)
+			{
+				arr[level] = i + 1;
+				//flag[i + 1] = 1;
+				recursive(level + 1);
+				arr[level] = 0;
+				//flag[i + 1] = 0;
+			}
+		}
+		else
+		{
+			if (flag[i + 1] == 0)
+			{
+				arr[level] = i + 1;
+				//flag[i + 1] = 1;
+				recursive(level + 1);
+				arr[level] = 0;
+				//flag[i + 1] = 0;
+			}
+		}
+		
+	}
+
+}
+
+int main()
+{
+	scanf("%d %d", &n, &m);
+	recursive(0);
+
+	return 0;
+}
