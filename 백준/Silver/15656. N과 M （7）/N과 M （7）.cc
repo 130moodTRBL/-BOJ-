dd89a87b7d45a6a189e3625bd0e56ac6180c4356
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-09 23:28:42 +0900

    [Silver III] Title: N과 M (7), Time: 688 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[8];
+int flag[9] = { 0, };
+int brr[8];
+void recursive(int level)
+{
+	int i;
+	if (level == m) {
+		for (i = 0; i < m; i++) {
+			printf("%d ", brr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < n; i++) {
+		
+			brr[level] = arr[i];
+			flag[i + 1] = 1;
+			recursive(level + 1);		//1 7 8 9
+			brr[level] = 0;
+			flag[i + 1] = 0;
+		
+	}
+}
+
+int main()
+{
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + n);
+	recursive(0);
+
+	return 0;
+}
