b307053a4e822cf0f45208c4481b91b4695038ea
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-10 17:17:59 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <stdlib.h>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	printf("고려대학교");
+
+	return 0;
+}
