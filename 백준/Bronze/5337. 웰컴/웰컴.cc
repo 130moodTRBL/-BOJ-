7c58cbe62a3ec682faa00f214ff8ffef76df0729
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-16 23:13:06 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+int arr[500001];
+int sorted[500001];
+long long val = 0;
+
+void merges(int left, int right) 
+{
+	int mid = (left + right) / 2;
+	int i = left;
+	int j = mid + 1;
+	int k = left;
+	long long cnt = 0;
+
+	while (i <= mid && j <= right) 
+	{
+		if (arr[i] <= arr[j]) {
+			sorted[k++] = arr[i++];
+			val += cnt;
+		}
+		else {
+			sorted[k++] = arr[j++];
+			cnt++;
+		}
+	}
+
+	if (i > mid) {
+		while (j <= right) 
+		{
+			sorted[k++] = arr[j++];
+			cnt++;
+		}
+	}
+	else {
+		while (i <= mid) 
+		{;
+			sorted[k++] = arr[i++];
+			val += cnt;
+		}
+	}
+
+	for (int a = left; a <= right; a++) {
+		arr[a] = sorted[a];
+	}
+}
+
+void mergeSort(int left, int right) 
+{
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		mergeSort(left, mid);
+		mergeSort(mid + 1, right);
+		merges(left, right);
+	}
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	/*int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	mergeSort(0, N - 1);
+	cout << val;*/
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+	return 0;
+}
