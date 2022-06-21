bcd44de425b7d156c496093700367e35c289345d
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-21 14:02:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	int n;
+	int dp[12];
+	cin >> t;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	for (int i = 4; i < 12; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << dp[n] << endl;
+	}
+
+	
+
+
+	return 0;
+}
