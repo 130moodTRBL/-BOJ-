962ddf89c0cf06e73c059a97df8b0974eea20da4
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-23 16:46:57 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char s[50];
+	scanf("%s", s);
+	printf("%s??!\n", s);
+
+
+
+	return 0;
+}
