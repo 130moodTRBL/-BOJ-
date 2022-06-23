f00031a5181b8a696d61a0f785d6b4f0986a39a8
130moodTRBL <101932293+130moodTRBL@users.noreply.github.com>
2022-06-23 16:48:03 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main() 
+{
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+
+
+
+	return 0;
+}
