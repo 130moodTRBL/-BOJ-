eece26f70d2e430e470e4cc757451dbc724ebd45
김선우 <101932293+130moodTRBL@users.noreply.github.com>
2022-07-25 22:10:06 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <tuple>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	long long a, b;
+	cin >> a >> b;
+	cout << (a + b) * (a - b);
+	
+
+
+
+
+	return 0;
+}
