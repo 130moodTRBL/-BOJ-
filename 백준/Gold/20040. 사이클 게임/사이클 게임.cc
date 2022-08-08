4de7c6e8065d9868180b29511aacca214b14a5a9
김선우 <101932293+130moodTRBL@users.noreply.github.com>
2022-08-08 14:13:24 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+
+#define endl '\n'
+using namespace std;
+
+int parent[1000001];
+
+int getParent(int x)
+{
+	if (parent[x] == x) {
+		return x;
+	}
+	return parent[x] = getParent(parent[x]);
+}
+
+void unionParent(int a, int b)
+{
+	a = getParent(a);
+	b = getParent(b);
+	if (a > b) {
+		parent[a] = b;
+	}
+	else {
+		parent[b] = a;
+	}
+}
+
+int find(int a, int b)
+{
+	a = getParent(a);
+	b = getParent(b);
+	if (a == b) {
+		return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	int a, b;
+	int ans = 0;
+	int check = 0;
+	cin >> n >> m;
+
+	for (int i = 0; i <= n; i++) {
+		parent[i] = i;
+	}
+
+	for (int i = 1; i <= m; i++) {
+		cin >> a >> b;
+		if (find(a, b) && check == 0) {
+			ans = i;
+			break;
+		}
+		unionParent(a, b);
+	}
+
+	cout << ans;
+
+
+
+	return 0;
+}
