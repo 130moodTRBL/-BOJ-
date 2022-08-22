e899ed55b7ff66c4428ae7a625c8e8a87ed7e9eb
김선우 <101932293+130moodTRBL@users.noreply.github.com>
2022-08-22 16:48:37 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << (56 * a) + (24 * b) + (14 * c) + (6 * d);
+
+
+
+
+	return 0;
+}
