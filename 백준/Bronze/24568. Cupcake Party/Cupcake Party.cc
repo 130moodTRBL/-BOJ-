a14e0dab96b50fdffa56eedf945f5dc6fd793782
김선우 <101932293+130moodTRBL@users.noreply.github.com>
2022-09-10 13:55:45 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << (a * 8) + (b * 3) - 28;
+
+
+
+
+	return 0;
+}
