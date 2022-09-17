4a2ed7b35149c081e2430136734c2bc9f7b6b3aa
김선우 <101932293+130moodTRBL@users.noreply.github.com>
2022-09-17 14:03:39 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << 192 << endl << "sonic6679";
+
+
+
+
+	return 0;
+}
